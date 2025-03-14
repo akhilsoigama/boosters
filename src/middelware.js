8d5c95f3beafe7c0ad5b14e5ca21666900de76a1
akhil soigama <akhilsoigama@gmail.com>
2025-03-14 10:45:58 +0530

    middelware commited

--- src/middelware.js
+++ src/middelware.js
@@ -1,17 +1,17 @@
-import { NextResponse } from 'next/server';
+// import { NextResponse } from 'next/server';
 
-const protectedRoutes = ['/', '/dashboard', '/profile'];
+// const protectedRoutes = ['/', '/dashboard', '/profile'];
 
-export async function middleware(req) {
-  const { pathname } = req.nextUrl;
+// export async function middleware(req) {
+//   const { pathname } = req.nextUrl;
 
-  if (protectedRoutes.includes(pathname)) {
-    const token = req.cookies.get('token')?.value;
+//   if (protectedRoutes.includes(pathname)) {
+//     const token = req.cookies.get('token')?.value;
+   
+//     if (!token) {
+//       return NextResponse.redirect(new URL('/Auth/signup', req.url));
+//     }
+//   }
 
-    if (!token) {
-      return NextResponse.redirect(new URL('/Auth/signup', req.url));
-    }
-  }
-
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
